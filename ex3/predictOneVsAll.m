a719aad32ad265e37559218bc72436eccbd25268
Nitish Agrawal <61539464+Nitishagrawal507@users.noreply.github.com>
2020-03-25 20:03:24 +0530

    Update predictOneVsAll.m

--- ex3/predictOneVsAll.m
+++ ex3/predictOneVsAll.m
@@ -1,40 +1,43 @@
-function p = predictOneVsAll(all_theta, X)
-%PREDICT Predict the label for a trained one-vs-all classifier. The labels 
-%are in the range 1..K, where K = size(all_theta, 1). 
-%  p = PREDICTONEVSALL(all_theta, X) will return a vector of predictions
-%  for each example in the matrix X. Note that X contains the examples in
-%  rows. all_theta is a matrix where the i-th row is a trained logistic
-%  regression theta vector for the i-th class. You should set p to a vector
-%  of values from 1..K (e.g., p = [1; 3; 1; 2] predicts classes 1, 3, 1, 2
-%  for 4 examples) 
+function p = predict(Theta1, Theta2, X)
+%PREDICT Predict the label of an input given a trained neural network
+%   p = PREDICT(Theta1, Theta2, X) outputs the predicted label of X given the
+%   trained weights of a neural network (Theta1, Theta2)
 
+% Useful values
 m = size(X, 1);
-num_labels = size(all_theta, 1);
+num_labels = size(Theta2, 1);
 
 % You need to return the following variables correctly 
 p = zeros(size(X, 1), 1);
 
-% Add ones to the X data matrix
-X = [ones(m, 1) X];
-
 % ====================== YOUR CODE HERE ======================
 % Instructions: Complete the following code to make predictions using
-%               your learned logistic regression parameters (one-vs-all).
-%               You should set p to a vector of predictions (from 1 to
-%               num_labels).
+%               your learned neural network. You should set p to a 
+%               vector containing labels between 1 to num_labels.
 %
-% Hint: This code can be done all vectorized using the max function.
-%       In particular, the max function can also return the index of the 
-%       max element, for more information see 'help max'. If your examples 
-%       are in rows, then, you can use max(A, [], 2) to obtain the max 
-%       for each row.
-%       
+% Hint: The max function might come in useful. In particular, the max
+%       function can also return the index of the max element, for more
+%       information see 'help max'. If your examples are in rows, then, you
+%       can use max(A, [], 2) to obtain the max for each row.
+%
 
+a1 = [ones(m, 1) X];
+%size(a1)
+%size(Theta1)
 
+z2 = a1 * Theta1';
+a2 = sigmoid(z2);
+%size(a2)
 
+a2 = [ones(size(a2,1), 1) a2];
 
+z3 = a2 * Theta2';
+a3 = sigmoid(z3);
+%size(a3)
 
+[val, index] = max(a3,[],2);
 
+p = index;
 
 % =========================================================================
 
