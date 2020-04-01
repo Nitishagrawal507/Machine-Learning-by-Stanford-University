d97fd01463055d79a6f01d5ef14924c846df7c2e
Nitish Agrawal <61539464+Nitishagrawal507@users.noreply.github.com>
2020-04-01 13:29:08 +0530

    Update recoverData.m

--- ex7/recoverData.m
+++ ex7/recoverData.m
@@ -1,3 +1,4 @@
+  
 function X_rec = recoverData(Z, U, K)
 %RECOVERDATA Recovers an approximation of the original data when using the 
 %projected data
@@ -21,7 +22,12 @@ X_rec = zeros(size(Z, 1), size(U, 1));
 %               Notice that U(j, 1:K) is a row vector.
 %               
 
-
+for i = 1 : size(Z, 1)
+    v = Z(i, :)';
+    for j=1: size(U, 1)
+        X_rec(i, j) = v' * U(j, 1:K)';
+    end
+end
 
 % =============================================================
 
